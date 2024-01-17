b6429490e380a1de12351d7719b86c871e82f38a
Daan van Dijk <daan.vandijk@qbuzz.nl>
2024-01-17 08:37:18 +0100

    First commit of sample repo for mod build

---
+++ first/src/main/java/org/example/SomeRecord.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record SomeRecord(int id, String name) {
+}
---
+++ second/src/main/java/org/example/SomeMain.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SomeMain {
+
+    public static void main(String[] args) {
+        SomeRecord someRecord = new SomeRecord(1, "test");
+        System.out.println(someRecord);
+    }
+
+}
